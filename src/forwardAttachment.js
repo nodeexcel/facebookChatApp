93e5330fbf12dc94ccc6b55c15fb13a925a3baa4
nodeexcel <atul.etech2011@gmail.com>
2024-07-04 19:28:57 +0530

    refactored  in forrwardAttackments

--- src/forwardAttachment.js
+++ src/forwardAttachment.js
@@ -13,11 +13,11 @@ module.exports = function(defaultFuncs, api, ctx) {
       attachment_id: attachmentID
     };
     
-    var timestamp = Math.floor(Date.now() / 1000);
     if (utils.getType(userOrUsers) !== "Array") {
       userOrUsers = [userOrUsers];
     }
-
+    
+    var timestamp = Math.floor(Date.now() / 1000);
 
     for (var i = 0; i < userOrUsers.length; i++) {
       //That's good, the key of the array is really timestmap in seconds + index
