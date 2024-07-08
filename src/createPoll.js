558c6e32d4500cb4fca86d973863446b09857890
nodeexcel <atul.etech2011@gmail.com>
2024-07-08 19:50:31 +0530

    arranging the variables

--- src/createPoll.js
+++ src/createPoll.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var utils = require("../utils");
 var log = require("npmlog");
+var utils = require("../utils");
 
 module.exports = function (defaultFuncs, api, ctx) {
   return function createPoll(title, threadID, options, callback) {
